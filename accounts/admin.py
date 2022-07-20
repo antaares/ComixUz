8d946f03186d52b7f6a66483a0d0c4f0dbb831df
antaares <jrismoilov@gmail.com>
2022-07-20 11:51:33 +0500

    first commit

---
+++ accounts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'age', 'is_staff',)
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    # 'is_staff', 'is_superuser', 'is_active', 'date_joined', 'last_login')
+
+admin.site.register(CustomUser, CustomUserAdmin)
